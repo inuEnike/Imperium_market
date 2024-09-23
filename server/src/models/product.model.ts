c10cc9368b17aa80d07f9eb3b5a67196a2fd4c73
Ennygenius <inuenike@gmail.com>
2024-09-23 01:33:33 -0700

    Created Product model

---
+++ server/src/models/product.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { model, Schema, Document, Types } from "mongoose";
+
+interface IReport extends Document {
+  seller: Schema.Types.ObjectId;
+  name: string;
+  price: string;
+  description: string;
+  imageURI: [string];
+  location: string;
+  condition: string;
+}
+
+const ProductSchema = new Schema<IReport>(
+  {
+    seller: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+    price: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    imageURI: {
+      type: [String],
+    },
+    location: {
+      type: String,
+    },
+    condition: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Product = model("product", ProductSchema);
